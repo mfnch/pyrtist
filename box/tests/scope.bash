1c640bb43e364e4c547b4b2f372a7a0e3345472f
fnch <devnull@localhost>
2006-03-18 16:33:37 +0000

    Minor changes

--- box/tests/scope.bash
+++ box/tests/scope.bash
@@ -1,76 +1,9 @@
 #!/bin/bash
-BOX="../box"
-BOXOPT="-st"
-BOXOUT="output.log"
-ERRORS="errors.log"
-NUM_TEST=0
-NUM_ERRORS=0
-ERR_REPORTED=0
 
-function err_report {
-  if [ $ERR_REPORTED -eq 0 ]; then
-    echo "*** Beginning of test $NUM_TEST (ERRORS DETECTED!) ***" >> $ERRORS
-    cat $BOXOUT >> $ERRORS
-    echo "*** End of test $NUM_TEST ***" >> $ERRORS
-    echo >> $ERRORS
-    ERR_REPORTED=1
-  fi
-}
-
-function test_next {
-  NUM_TEST=$[ $NUM_TEST + 1 ]
-  echo
-  echo -n "  < Test $NUM_TEST: "
-  ERR_REPORTED=0
-}
-
-function check_successful {
-  echo "  > Test successful!"
-}
-
-function check_failed {
-  echo "  > --> Test FAILED! <--"
-}
-
-function check_noerr {
-  local ne=$(grep Err $BOXOUT | wc -l)
-  NUM_ERRORS=$[ $NUM_ERRORS + $ne ]
-  if [ $ne -lt 1 ]; then
-    check_successful
-  else
-    err_report
-    check_failed
-  fi
-}
-
-function check_answer {
-  local ANSWER=$(grep "answer=" $BOXOUT | cut -d = -f 2)
-  if [ "$ANSWER" == "$1" ]; then
-    check_successful
-  else
-    err_report
-    check_failed
-  fi
-}
-
-function check_final {
-  echo "$NUM_ERRORS errors found."
-  if [ $NUM_ERRORS -lt 1 ]; then
-    echo "All tests performed led to success."
-    exit 0
-  fi
-  echo "See the file '$ERRORS' for more details!"
-  exit 1
-}
+. common.bash
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
-if [ -x $BOX ]; then
-  BOX="$BOX $BOXOPT"
-else
-  echo "I cannot find the executable file '$BOX'!"
-  exit 1
-fi
 
 echo "<< Testing if explicit variables are killed when their box is closed:"
 rm -f $ERRORS
