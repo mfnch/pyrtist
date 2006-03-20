63e4754ce29261fcce17a0c0f5394132a363e73c
fnch <devnull@localhost>
2006-03-20 00:16:48 +0000

    Changes in comments

--- box/compiler/compiler.c
+++ box/compiler/compiler.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005 Franchin Matteo
+ * Copyright (c) 2005 Matteo Franchin
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
