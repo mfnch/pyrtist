97ad3147d6b279f086d71694a23ff04748f833c3
fnch <devnull@localhost>
2008-03-23 19:45:33 +0000

    Some examples.

---
+++ box/examples/smileys/bouncing.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function cmnd {
+  echo "$*"
+  $*
+}
+
+BOX_FILE="bouncing.box"
+TMP_DIR="./bouncing"
+TMP_FILE="tmp.box"
+
+mkdir -p $TMP_DIR
+cd $TMP_DIR
+
+FILE_LIST=""
+for (( i = 0; i < 120; i += 1 )); do
+  cat << EOF > $TMP_FILE
+  t = 0.025*$i
+EOF
+  cat ../$BOX_FILE >> $TMP_FILE
+
+  cmnd box -l g $TMP_FILE
+  NEW_FILE="bouncing_$i.gif"
+  cmnd convert bouncing.bmp -scale 80x80 $NEW_FILE
+  FILE_LIST="$FILE_LIST $NEW_FILE"
+done
+
+cmnd gifsicle --delay 1 $FILE_LIST --output=../bouncing.gif
+cd ..
+
