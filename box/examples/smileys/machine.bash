2b75c3169d64870a260a77b4169623b7a4b72e30
fnch <devnull@localhost>
2008-04-10 20:28:42 +0000

    More examples.

--- box/examples/smileys/machine.bash
+++ box/examples/smileys/machine.bash
@@ -5,26 +5,26 @@ function cmnd {
   $*
 }
 
-BOX_FILE="bouncing.box"
-TMP_DIR="./bouncing"
+BOX_FILE="machine.box"
+TMP_DIR="./machine"
 TMP_FILE="tmp.box"
 
 mkdir -p $TMP_DIR
 cd $TMP_DIR
 
 FILE_LIST=""
-for (( i = 0; i < 120; i += 2 )); do
+for (( i = 0; i < 200; i += 4 )); do
   cat << EOF > $TMP_FILE
-  t = 0.025*$i
+  wa = 0.1*$i
 EOF
   cat ../$BOX_FILE >> $TMP_FILE
 
   cmnd box -l g $TMP_FILE
-  NEW_FILE="bouncing_$i.gif"
-  cmnd convert bouncing.bmp -scale 80x80 $NEW_FILE
+  NEW_FILE="pump_$i.gif"
+  cmnd convert machine.bmp -scale 100x100 $NEW_FILE
   FILE_LIST="$FILE_LIST $NEW_FILE"
 done
 
-cmnd gifsicle -l0 -d1 $FILE_LIST --output=../bouncing.gif
+cmnd gifsicle -l0 -d1 $FILE_LIST --output=../machine.gif
 cd ..
 
