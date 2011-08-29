82415022af89f863c0766354fc8feefa853a97ac
Matteo Franchin <fnch@users.sourceforge.net>
2011-08-29 18:46:00 +0100

    Adding a Transform object, to specify transformation/matrices easily.

--- box/libs/g/transform.c
+++ box/libs/g/transform.c
@@ -48,62 +48,32 @@
 
 /*#define DEBUG*/
 
-#if 0
-/* Quantita' che definiscono la trasformazione */
-static Real Qx, Qy, Tx, Ty;
-static Real theta, cos_theta, sin_theta, s, cos_tau, sin_tau/*,tau*/;
 
-#define AUTO_TRANSLATION(n) ( ((n) & 0x3) != 0 )
-#define AUTO_TRANSLATION_X(n) ( ((n) & 0x1) != 0 )
-#define AUTO_TRANSLATION_Y(n) ( ((n) & 0x2) != 0 )
-#define AUTO_ROTATION(n) ( ((n) & 0x4) != 0 )
-#define AUTO_SCALE(n) ( ((n) & 0x8) != 0 )
-#define AUTO_PROPORTION(n) ( ((n) & 0x10) != 0)
-#define AUTO_INVERSION(n) ( ((n) & 0x20) != 0)
-#define FIXED_PROPORTION(n) ( ((n) & 0x30) == 0)
+const char *BoxGAutoTransformErr_To_String(BoxGAutoTransformErr n) {
+  switch (n) {
+  case BOXGAUTOTRANSFORMERR_NO_ERR:
+    return NULL;
+  case BOXGAUTOTRANSFORMERR_NOT_ENOUGH_POINTS:
+    return "Need more points for the required transformation.";
+  case BOXGAUTOTRANSFORMERR_ZERO_WEIGHTS:
+    return "Error: weights sum up to zero.";
+  case BOXGAUTOTRANSFORMERR_NOT_IMPLEMENTED:
+    return "Case not implemented, yet.";
+  }
 
-#define ONLY_TRANSLATION(n) ( (n & ~0x3) == 0 )
+  return "Unknown error.";
+}
 
-#define SET_TRANSLATION_X(n, mode)  ( n = (n & ~0x1) | (mode & 0x1) )
-#define SET_TRANSLATION_Y(n, mode)  ( n = (n & ~0x2) | (mode & 0x2) )
-#define SET_ROTATION(n, mode)  ( n = (n & ~0x4) | (mode & 0x4) )
-#define SET_SCALE(n, mode)    ( n = (n & ~0x8) | (mode & 0x8) )
-#define SET_DEFORM(n, mode)    ( n = (n & ~0x10) | (mode & 0x10) )
-#define SET_INVERSION(n, mode)  ( n = (n & ~0x20) | (mode & 0x20) )
-#endif
-
-typedef enum {
-  BOXGAUTOTRANSFORMERR_NO_POINTS = 1,
-  BOXGAUTOTRANSFORMERR_ZERO_WEIGHTS,
-  BOXGAUTOTRANSFORMERR_NOT_IMPLEMENTED
-} BoxGAutoTransformErr;
-
-
-/* DESCRIZIONE: Questa funzione serve a posizionare una figura.
- *  Essa esegue i calcoli necessari per determinare come la figura debba essere
- *  traslata, ruotata e/o ridimensionata per soddisfare i vincoli imposti
- *  dall'utente. Il numero di vincoli e' specificato da n e ciascuno di essi
- *  comprende 3 argomenti:
- *    vincolo n-esimo ---> ( F[n], R[n], weight[n] )
- *  F[n] e' un punto della figura, R e' un punto di Riferimento dello sfondo
- *  (sfondo = "foglio" su cui verra' disposta la figura), mentre weight
- *  e' un numero reale che specifica la "forza" del vincolo.
- *  Ogni vincolo puo' essere pensato come una "molla" che collega F con R:
- *  la figura ruotera'/traslera' e/o si ridimensionera' in modo che le molle
- *  si accorcino. weight e' la forza della molla, e serve per rendere un vincolo
- *  (= una molla) piu' importante degli altri.
- *  L'utente puo' decidere quali saranno le "liberta'" dell'oggetto.
- *  Puo' cioe' stabilire se le molle provocheranno solo una rotazione
- *  dell'oggetto o se provocheranno solo una sua traslazione, o ridimensionamento,
- *  o una qualsiasi combinazione di tali trasformazioni.
- *  Le liberta' sono determinate dall'argomento chiamato needed, secondo
- *  lo schema seguente:
- *   BIT 0 di needed  -->  L'oggetto traslera' lungo X (automaticamente)
- *   BIT 1 di needed  -->  L'oggetto traslera' lungo Y (automaticamente)
- *   BIT 2 di needed  -->  L'oggetto ruotera' (automaticamente)
- *   BIT 3 di needed  -->  L'oggetto sara' ridimensionato (automaticamente)
- *   BIT 4 di needed  -->  L'oggetto sara' "sproporzionato" (automaticamente)
- *   BIT 5 di needed  -->  L'oggetto sara' riflesso (automaticamente)
+/** From an initial transformation 'transform', 'n' constraints (provided in
+ * 'src', 'dst' and 'weight') and a set of allowed transformations,
+ * 'allowed_transformation', finds the one which best satisfies the
+ * constraints. In particular, the objective is to minimise the function:
+ *
+ *   f(q) = sum_i {weight[i]*(T(q) src[i] - dst[i])^2} / sum_i {weight[i]}
+ *
+ * where T[q] is a transformation parametrized by q, which is a vector made
+ * by all the available degrees of freedom. The objective is then to determine
+ * 'q' and T(q). T(q) is then stored inside 'transform'.
  */
 BoxGAutoTransformErr
 BoxG_Auto_Transform(BoxGTransform *transform,
@@ -113,7 +83,7 @@ BoxG_Auto_Transform(BoxGTransform *transform,
   BoxReal weights_sum;
 
   if (n < 1)
-    return BOXGAUTOTRANSFORMERR_NO_POINTS;
+    return BOXGAUTOTRANSFORMERR_NOT_ENOUGH_POINTS;
 
 #ifdef DEBUG
   for (i = 0; i < n; i++) {
@@ -270,6 +240,76 @@ BoxG_Auto_Transform(BoxGTransform *transform,
   return 1;
 }
 
+BoxTask BoxGAllow_Of_String(BoxGAllow *allow, const char *string) {
+  char p;
+  enum {NORMAL, WAIT_COMPONENT} state = NORMAL;
+  BoxGAllow my_allow = 0;
+  int do_set = 1;
+
+  p = tolower(*string);
+  if (p == ' ')
+    my_allow = *allow;
+
+  for (;; p = tolower(*string)) {
+    BoxGAllow queued_flag = 0;
+
+    switch (state) {
+    case NORMAL:
+      switch (p) {
+      case 't':
+        state = WAIT_COMPONENT; break;
+      case 'r':
+        queued_flag = BOXGALLOW_ROTATE; break;
+      case 's':
+        queued_flag = BOXGALLOW_SCALE; break;
+      case 'a':
+        queued_flag = BOXGALLOW_ANISOTROPIC; break;
+      case 'i':
+        queued_flag = BOXGALLOW_INVERT; break;
+      case ' ':
+        break;
+      case '+':
+        do_set = 1; break;
+      case '-':
+        do_set = 0; break;
+      case '\0':
+        *allow = my_allow;
+        return BOXTASK_OK;
+      default:
+        return BOXTASK_FAILURE;
+      }
+      ++string;
+      break;
+
+    case WAIT_COMPONENT:
+      switch (p) {
+      case 'x':
+        queued_flag = BOXGALLOW_TRANSLATE_X;
+        ++string; 
+        break;
+      case 'y':
+        queued_flag = BOXGALLOW_TRANSLATE_Y;
+        ++string;
+        break;
+      default:
+        queued_flag = BOXGALLOW_TRANSLATE;
+        break;
+      }
+      state = NORMAL;
+      break;
+    }
+
+    /* Set or reset the queued flag */
+    if (do_set)
+      my_allow |= queued_flag;
+    else
+      my_allow &= ~queued_flag;
+  }
+
+  abort(); /* We should never get here! */
+  return BOXTASK_FAILURE;
+}
+
 #if 0
 /* DESCRIZIONE: Setta i parametri della trasformazione prima di avviare
  *  il calcolo automatico con aput_autoput.
@@ -304,90 +344,5 @@ void aput_get(Point *rot_center, Point *trsl_vect,
   *rot_angle = theta;
   *scale_x = s * cos_tau;
   *scale_y = s * sin_tau;
-}
-
-/* DESCRIZIONE: Converte una stringa in un valore di "needed" da usare nella
- *  funzione aput_autoput(). La stringa specifica quali trasformazioni
- *  debbano essere eseguite automaticamente e quali manualmente.
- *  Essa e' costituita da una successione di lettere, ciascuna delle quali
- *  specifica una trasformazione da eseguire. Se - ad esempio -
- *  voglio traslare e ruotare automaticamente, allora permission = "tr"
- *  oppure = "rt" (l'ordine non conta!). Le associazioni lettere-trasform. sono:
- *   "t" -> traslazione, "r" -> rotazione, "s" -> scala,
- *   "d" -> deformazione (ovvero: cambio di proporzioni X-Y),
- *   "i" -> inversione (ovvero: riflessione)
- *  Si puo' anche usare "tx" per traslazione solo lungo x, analogamente "ty".
- *  E' possibile premettere a ciascuna lettera un segno: "-" e' opposto al "+",
- *  nel senso che "+" aggiunge, mentre "-" toglie la trasformazione
- *  (esempio "rt" = "+r+t"). Il segno vale per tutte le lettere seguenti.
- *  Se il primo carattere della stringa e' uno spazio, allora sara' continuata
- *  la stringa a cui corrisponde il valore needed.
- *  (esempio: se olp_perm = aput_allow("s", 0) e x = aput_allow(" d", old_perm),
- *  allora x = aput_allow("sd", 0) )
- *  Gli altri spazi della stringa vengono ignorati.
- */
-int aput_allow(const char *permissions, int *needed) {
-  char p;
-  int allow = 0;
-  enum {NORMAL, WAIT_COMPONENT} status = NORMAL;
-  enum {CLEAR=0, SET=-1} mode = SET;
-
-  p = tolower(*permissions);
-  if ( p == ' ' ) { allow = *needed; }
-
-  for (;; p = tolower(*permissions) ) {
-
-    switch (status) {
-    case NORMAL:
-      switch (p) {
-      case 't':
-        status = WAIT_COMPONENT; break;
-      case 'r':
-        SET_ROTATION(allow, mode); break;
-      case 's':
-        SET_SCALE(allow, mode); break;
-      case 'd':
-        SET_DEFORM(allow, mode); break;
-      case 'i':
-        SET_INVERSION(allow, mode); break;
-      case ' ':
-        break;
-      case '+':
-        mode = SET;
-        break;
-      case '-':
-        mode = CLEAR;
-        break;
-      case '\0':
-        *needed = allow;
-        return 1;
-      default:
-        ERRORMSG("aput_allow", "La lettera non corrisponde "
-                 "ad una trasformazione ammessa");
-        return 0;
-      }
-      ++permissions;
-      break;
-
-    case WAIT_COMPONENT:
-      switch (p) {
-      case 'x':
-        SET_TRANSLATION_X(allow, mode);
-        ++permissions; break;
-      case 'y':
-        SET_TRANSLATION_Y(allow, mode);
-        ++permissions; break;
-      default:
-        SET_TRANSLATION_X(allow, mode);
-        SET_TRANSLATION_Y(allow, mode);
-        break;
-      }
-      status = NORMAL;
-      break;
-    }
-
-  }
-
-  return 0;
 }
 #endif
--- box/libs/g/transform.h
+++ box/libs/g/transform.h
@@ -56,13 +56,60 @@ typedef struct {
 } BoxGTransform;
 
 #if 0
-void aput_identity_matrix(Real *matrix);
-void aput_get(Point *rot_center, Point *trsl_vect,
-              Real *rot_angle, Real *scale_x, Real *scale_y );
-void aput_set(Point *rot_center, Point *trsl_vect,
-              Real *rot_angle, Real *scale_x, Real *scale_y );
-int aput_autoput(Point *F, Point *R, Real *weight, int n, int needed);
-int aput_allow(const char *permissions, int *needed);
+Transform = ++(Point translation, rotation_center, scale_factors
+               Real rotation_angle)
 #endif
+
+/** Used by BoxG_Auto_Transform to signal errors. */
+typedef enum {
+  BOXGAUTOTRANSFORMERR_NO_ERR=0,
+  BOXGAUTOTRANSFORMERR_NOT_ENOUGH_POINTS,
+  BOXGAUTOTRANSFORMERR_ZERO_WEIGHTS,
+  BOXGAUTOTRANSFORMERR_NOT_IMPLEMENTED
+} BoxGAutoTransformErr;
+
+
+/** Provide a string representation of the given BoxGAutoTransformErr. */
+const char *BoxGAutoTransformErr_To_String(BoxGAutoTransformErr n);
+
+/** From an initial transformation 'transform', 'n' constraints (provided in
+ * 'src', 'dst' and 'weight') and a set of allowed transformations,
+ * 'allowed_transformation', finds the one which best satisfies the
+ * constraints. In particular, the objective is to minimise the function:
+ *
+ *   f(q) = sum_i {weight[i]*(T(q) src[i] - dst[i])^2} / sum_i {weight[i]}
+ *
+ * where T[q] is a transformation parametrized by q, which is a vector made
+ * by all the available degrees of freedom. The objective is then to determine
+ * 'q' and T(q). T(q) is then stored inside 'transform'.
+ */
+BoxGAutoTransformErr
+BoxG_Auto_Transform(BoxGTransform *transform,
+                    BoxPoint *src, BoxPoint *dst, BoxReal *weight, int n,
+		    BoxGAllow allowed_transforms);
+
+/** Convert a string to a BoxGAllow value to use with BoxG_Auto_Transform. This
+ * function allows quick specification of the allowed transformations. Allowed
+ * transformations are specified using letters. Each letter corresponds to an
+ * allowed transformation. For example: "rt" means that "r"otations and
+ * "t"ranslations are allowed. The order of letters does not matter, e.g. "rt"
+ * is equivalent to "tr". The following table collects the associations
+ * between letters and transformations:
+ *  "t" -> translation, "r" -> rotation, "s" -> scale,
+ *  "a" -> allows anisotropic scaling, "i" -> allows inversion (mirroring) It
+ * is also possible to use "tx" or "ty" to enable translation only along one of
+ * the axes. It is also possible to give sign "+" and "-". The sign change the
+ * behaviour of the function. "+" means that the following characters should be
+ * used to enable transformations (default), while "-" means that they should
+ * be used to disable transformations. This is handy for concatenated
+ * strings. Example: allowed + "-txr" means that we should do whathever
+ * specified by 'allowed', but we should not translate along-x nor rotate.
+ * There are then a few characters to fine tune the behaviour of the function.
+ * When 'string' starts with a white space " ", the flags are or-ed over the
+ * pre-existing flags in '*allow' (while if the string does not start with a
+ * space the initial value of '*allow' is ignored, i.e. set to zero). Spaces in
+ * the middle of the string are ignored.
+ */
+BoxTask BoxGAllow_Of_String(BoxGAllow *allow, const char *string);
 
 #endif /*_BOX_LIBG_TRANSFORM_H */
