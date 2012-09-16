d11dcc2419d2b69b1429519b5d0eb046a2d75837
Matteo Franchin <fnch@users.sf.net>
2012-09-16 18:30:25 +0100

    Adjusting doxygen prototype.

--- box/prototypes/doxygen.h
+++ box/prototypes/doxygen.h
@@ -35,15 +35,12 @@
 
 #  include <systemheader1.h>
 #  include <systemheader2.h>
-// ... other headers, with a final blank line
 
 #  include <box/header1.h>
 #  include <box/header2.h>
-// ... other headers, with a final blank line
 
 #  include "local_header1.h"
 #  include "local_header2.h"
-// ... other headers, with a final blank line
 
 /**
  * @brief Use brief, otherwise the index won't have a brief explanation.
@@ -68,25 +65,33 @@ typedef struct BoxStruct_struct {
 } BoxStruct;
 
 /**
+ * @brief Example showing how to document a function with Doxygen.
+ *
  * Description of what the function does. This part may refer to the parameters
  * of the function, like @p param1 or @p param2. A word of code can also be
  * inserted like @c this which is equivalent to <tt>this</tt> and can be useful
- * to say that the function returns a @c void or an @c int. When multiple words
- * should be written using typewriter font, just use @<tt@>.
- * We can also include text verbatim, @verbatim like this @endverbatim
+ * to say that the function returns a @c void or an @c int. If you want to have
+ * more than one word in typewriter font, then just use @<tt@>.
+ * We can also include text verbatim,
+ * @verbatim like this@endverbatim
  * Sometimes it is also convenient to include an example of usage:
  * @code
  * BoxStruct *out = Box_The_Function_Name(param1, param2);
+ * printf("something...\n");
  * @endcode
  * Or,
  * @code{.py}
  * pyval = python_func(arg1, arg2)
+ * print pyval
  * @endcode
- * when the language is not the one used in the current source file.
+ * when the language is not the one used in the current source file (but
+ * <b>be careful</b> as this may be supported only by recent versions
+ * of Doxygen). By the way, <b>this is how you write bold text</b> or,
+ * if it is just one word, then you can just do @b this.
  * @param param1 Description of the first parameter of the function.
  * @param param2 The second one, which follows @p param1.
  * @return Describe what the function returns.
- * @see Box_Another_One
+ * @see Box_The_Second_Function
  * @see Box_The_Last_One
  * @see http://website/
  * @note Something to note.
@@ -98,15 +103,16 @@ Box_The_Function_Name(BoxParamType1 param1, BoxParamType2 param2 /*, ...*/);
 /**
  * @brief A simple stub function to show how links do work.
  *
- * Automatic links are generated for webpages (like http//www.google.co.uk)
- * for typedef-ed structures, like BoxStruct or #BoxStruct.
+ * Links are generated automatically for webpages (like http://www.google.co.uk)
+ * and for structures, like BoxStruct_struct. For typedef-ed types use
+ * #BoxStruct.
  * For functions, automatic links are generated when the parenthesis () follow
  * the name of the function, like Box_The_Function_Name().
  * Alternatively, you can use #Box_The_Function_Name.
  * @return @c NULL is always returned.
  */
 BOXEXPORT void *
-Box_Another_One(void);
+Box_The_Second_Function(void);
 
 /**
  * Brief can be omitted. If you configure Doxygen with
