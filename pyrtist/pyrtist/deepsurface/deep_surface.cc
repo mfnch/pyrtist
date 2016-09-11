52fe39496b8863662620454ff08c5940697a26cf
Matteo Franchin <fnch@users.sf.net>
2016-09-11 12:43:49 +0100

    Implement alpha blending during transfer

--- pyrtist/pyrtist/deepsurface/deep_surface.cc
+++ pyrtist/pyrtist/deepsurface/deep_surface.cc
@@ -1,4 +1,32 @@
+#include <assert.h>
+
 #include "deep_surface.h"
+
+// Utility functions.
+static uint32_t GetA(uint32_t argb) { return (argb >> 24) & 0xff; }
+static uint32_t GetR(uint32_t argb) { return (argb >> 16) & 0xff; }
+static uint32_t GetG(uint32_t argb) { return (argb >> 8) & 0xff; }
+static uint32_t GetB(uint32_t argb) { return argb & 0xff; }
+static uint32_t GetARGB(uint8_t a, uint8_t r, uint8_t g, uint8_t b) {
+  return ((static_cast<uint32_t>(a) << 24) |
+          (static_cast<uint32_t>(r) << 16) |
+          (static_cast<uint32_t>(g) << 8) |
+          (static_cast<uint32_t>(b)));
+}
+
+static uint32_t BlendSrcOverDst(uint32_t src_argb, uint32_t dst_argb) {
+  uint32_t src_a = GetA(src_argb);
+  uint32_t dst_a = GetA(dst_argb);
+
+  uint32_t src_am1 = UINT32_C(0xff) - src_a;
+  uint32_t src_weight = src_a;
+  uint32_t dst_weight = (dst_a*src_am1)/UINT32_C(0xff);
+  uint32_t out_a = src_weight + dst_weight;
+  uint32_t out_r = (GetR(src_argb)*src_weight + GetR(dst_argb)*dst_weight)/out_a;
+  uint32_t out_g = (GetG(src_argb)*src_weight + GetG(dst_argb)*dst_weight)/out_a;
+  uint32_t out_b = (GetB(src_argb)*src_weight + GetB(dst_argb)*dst_weight)/out_a;
+  return GetARGB(out_a, out_r, out_g, out_b);
+}
 
 bool DeepSurface::SaveToFiles(const char* image_file_name,
                                const char* normals_file_name) {
@@ -31,25 +59,28 @@ void DeepSurface::Transfer(bool and_clear) {
   for (int iy = 0; iy < ny; iy++, offset += new_line) {
     size_t final_line_offset = offset + nx;
     for (; offset < final_line_offset; offset++) {
-      // Source is transparent. Nothing to do.
       uint32_t src_argb = isrc[offset];
-      uint8_t src_alpha = src_argb >> 24;
+      uint8_t src_alpha = GetA(src_argb);
       if (src_alpha == 0)
+        // Source is transparent. Nothing to do.
         continue;
 
-      // Source has infinite depth. Again, nothing to do.
       auto src_depth = dsrc[offset];
       if (DeepBuffer::IsInfiniteDepth(src_depth))
+        // Source has infinite depth. Again, nothing to do.
         continue;
 
       auto dst_depth = ddst[offset];
-      if (DeepBuffer::IsInfiniteDepth(dst_depth) || src_depth > dst_depth) {
+      if (DeepBuffer::IsInfiniteDepth(dst_depth) || src_depth >= dst_depth) {
         ddst[offset] = src_depth;
-        idst[offset] = src_argb;
-      } else if (src_depth < dst_depth) {
+        if (src_alpha == 0xff)
+          idst[offset] = src_argb;
+        else
+          idst[offset] = BlendSrcOverDst(src_argb, idst[offset]);
       } else {
+        assert(src_depth < dst_depth);
+        idst[offset] = BlendSrcOverDst(idst[offset], src_argb);
       }
-      //uint32_t dst_argb = idst[offset];
     }
   }
 
