b7e4b639988eeda5a9a6f0881d54af62b037a3ee
Matteo Franchin <fnch@users.sf.net>
2016-10-19 22:07:59 +0100

    Crescent implementation is feature complete
    Still need to fix precision issues for arcs with low curvature.

--- pyrtist/pyrtist/lib2deep/crescent.py
+++ pyrtist/pyrtist/lib2deep/crescent.py
@@ -2,7 +2,7 @@
 
 __all__ = ('Crescent',)
 
-from ..lib2d import Point, Window, Arc, Through, Close, Fill
+from ..lib2d import Point, Window, Arc, Through, Close, Fill, Radii
 from ..lib2d.base import combination
 from .core_types import Point3
 from .cmd_stream import Cmd
@@ -12,6 +12,9 @@ from .primitive import Primitive, InvalidPrimitiveError
 class Crescent(Primitive):
     def __init__(self, *args):
         self.points = []
+        self.scale_z = 1.0
+        self.z = None
+        self.radius_z = None
         super(Crescent, self).__init__(*args)
 
     def get_points(self):
@@ -19,20 +22,51 @@ class Crescent(Primitive):
             raise InvalidPrimitiveError('Crescent needs exactly 4 points')
         return self.points
 
+    def get_radius_z(self):
+        p1, p2, p3, p4 = self.get_points()
+        arc1 = Arc(Through(p1, p2, p3))
+        arc2 = Arc(Through(p3, p4, p1))
+        radius = (arc1[0.5] - arc2[0.5]).norm()*0.5
+        return radius
+
     def get_profile(self, *args):
-        p1, p2, p3, p4 = tuple(p.xy for p in self.get_points())
+        p1, p2, p3, p4 = self.get_points()
         return Fill(Arc(Through(p1, p2, p3), Close.no),
                     Arc(Through(p3, p4, p1), Close.no), *self.extra_args)
 
     def build_depth_cmd(self):
         p1, p2, p3, p4 = self.get_points()
-        origin = Point3(((p1 + p3)*0.5).xy, p1.z)
-        one_zero = p3
+        origin = Point3((p1 + p3)*0.5, self.z or 0.0)
+        one_zero = Point3(p3, origin.z + self.get_radius_z()*self.scale_z)
         zero_one = origin + Point3((one_zero - origin).xy.ort())
-        return [Cmd(Cmd.on_crescent, origin, one_zero, zero_one, p2, p4)]
+        return [Cmd(Cmd.on_crescent, origin, one_zero, zero_one,
+                    Point3(p2), Point3(p4))]
 
 
 @combination(Point, Crescent)
-@combination(Point3, Crescent)
 def point_at_crescent(point, crescent):
-    crescent.points.append(Point3(point))
+    crescent.points.append(point)
+
+@combination(Point3, Crescent)
+def point3_at_crescent(point3, crescent):
+    if crescent.z is None:
+        crescent.z = point3.z
+    else:
+        if point3.z != crescent.z:
+            raise ValueError('All Point3 given to Crescent should have '
+                             'the same z')
+    crescent.points.append(point3.xy)
+
+@combination(int, Crescent)
+@combination(float, Crescent)
+def scalar_at_crescent(scalar, crescent):
+    crescent.scale_z = scalar
+
+@combination(Radii, Crescent)
+def radii_at_crescent(radii, crescent):
+    radii.check(1, 1)
+    radius, = radii
+    if radius <= 0.0:
+        raise ValueError('Crescent only takes positive radius. '
+                         'Use a negative scale to invert the depth along z.')
+    crescent.radius_z = radius
