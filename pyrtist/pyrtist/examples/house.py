6273ee2a806dfa6de2c511c26da5c27748eaf72f
Matteo Franchin <fnch@users.sf.net>
2016-11-19 09:39:08 +0000

    Fix comments in house example

--- pyrtist/pyrtist/examples/house.py
+++ pyrtist/pyrtist/examples/house.py
@@ -88,9 +88,9 @@ w17 = Point(26.7301843088, 22.0110539054)
 # I had in mind the typical house here in England...
 
 # We definitely need a brick pattern to draw the walls
-# Try to change to gui(bricks)	 to see how this is done.
+# Try to change to gui(bricks) to see how this is done.
 
-# Just one brick
+# Just one brick.
 brick = Window()
 brick.take(
   Poly(Color(1.000, 0.496, 0.000), 0.5,
@@ -98,17 +98,20 @@ brick.take(
   Hot("center", p11)
 )
 
-# A brick pattern, which we save to the file "bricks.png"
+# A brick pattern, which we save as a cairo surface.
 bricks = Window()
 bricks.take(Put(brick),
             Put(brick, "t", Near("center", p12)),
             BBox(bbox1, bbox2))
 surface = bricks.draw(mode="rgb24", resolution=50/25.4)
 
+# We make a proper pattern using the cairo surface.
+pat = Image(surface, Scale(3.5), Extend.reflect, Filter.best)
+
 house = Window()
 house.BBox(bbox3, bbox4)
 
-# Colors used below
+# Colors used below.
 panes_color = Color(0.85, 0.95, 1)
 window_color = Grey(1)
 door_color = Color(0.545, 0.272, 0.078)
@@ -117,7 +120,7 @@ border = Border(Color.black, 0.5, Join.round, Cap.round)
 roof_color_bright = Color(0.9, 0.35, 0.3)
 roof_color = Color(0.647, 0.165, 0.165)
 
-# Roof
+# Roof.
 house.take(
   Poly(r1, r2, r3, r4, r5, r6, Style(roof_color, border)),
   Line(r7, r8, r9, roof_color_bright, Border(1, Cap.round)),
@@ -128,21 +131,18 @@ house.take(
 # Just to make the floor more regular...
 q4.y = q5.y = q8.y = q1.y
 
-# Draw the wall using the brick pattern
-pat = Image(surface, Scale(3.5), Extend.reflect, Filter.best)
-
-# Now draw the door
+# Now draw the door.
 house.take(
   Poly(q8, q7, q6, q5, q5, Style(border, door_color)),
   Poly(q10, q11, q12, q13, Style(border, door_color.darken(0.6))),
   Poly(q14, q15, q16, q17, Style(border, door_color.darken(0.6))),
   Poly(q1, q2, q9, q3, q4, q5, q6, q7, q8, Style(pat, border)),
-  # And the door handle
+  # And the door handle.
   Circle(q18, 1),
   Circle(q19, 0.3, Grey(0.5))
 )
 
-# Create a window
+# Create a window.
 window = Window()
 window_marks = Border(Color.black, 0.3, Cap.round)
 window.take(
@@ -157,7 +157,7 @@ window.take(
 window.take(Line(window_marks, *ab)
             for ab in [(w10, w11), (w12, w13), (w14, w15), (w16, w17)])
 
-# And draw it twice
+# And draw it twice.
 house.take(
   Put(window),
   Put(window, "t", Near("center", p26))
